0c9bae9fccad865093fcbe72306acc25a682820c
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-11-26 03:25:59 +0500

    asdad

--- src/core/config/firebase.js
+++ src/core/config/firebase.js
@@ -1,9 +1,34 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const path = require('path');
+
+let serviceAccount;
+
+const possiblePaths = [
+  './serviceAccountKey.json',
+  '../../../serviceAccountKey.json',
+  '../../serviceAccountKey.json',
+  '../serviceAccountKey.json',
+  './config/serviceAccountKey.json',
+];
+
+for (const filePath of possiblePaths) {
+  try {
+    const fullPath = path.resolve(__dirname, filePath);
+    serviceAccount = require(fullPath);
+    console.log(`✅ Firebase service account загружен из: ${fullPath}`);
+    break;
+  } catch (err) {
+    continue;
+  }
+}
+
+if (!serviceAccount) {
+  throw new Error('Firebase serviceAccountKey.json не найден ни по одному из путей!');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: serviceAccount.project_id + ".appspot.com"
+  storageBucket: serviceAccount.project_id + '.appspot.com',
 });
 
 const bucket = admin.storage().bucket();
