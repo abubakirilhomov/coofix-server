88cca9c6928b689048c6a846bff00d75d5d15348
Abubakir <147513879+abubakirilhomov@users.noreply.github.com>
2025-12-09 14:31:18 +0500

    Restrict CORS to specific origins

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 const app = express();
 
 app.use(cors({
-  origin: "*",
+  origin: ["http://localhost:3000", "http://localhost:3001"],
   methods: "GET,POST,PUT,PATCH,DELETE",
   allowedHeaders: "Content-Type, Authorization"
 }));
