842c2512c5554ae3398b3fdbc7b65e1817f61752
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-12-15 15:21:47 +0500

    adsaa

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "https://coofix-admin-dashboard.netlify.app/", "https://coofix-admin-dashboard.netlify.app", "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
