cfefb9cd3f9ec8ff7849a0f0117b5c1788569d26
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-12-29 18:58:48 +0500

    Asda

--- src/modules/users/user.model.js
+++ src/modules/users/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  lastName: { type: String, required: true },
+  lastName: { type: String},
 
   phone: {
     type: String,
